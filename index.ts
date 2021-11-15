8478ec03b019a338ce83415d073b60c48774e72f
Yixian Du <duyixian1234@qq.com>
2021-11-15 16:46:39 +0800

    check exist `.gitignore` file

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 const BASE_URL = "https://www.toptal.com/developers/gitignore/api/";
 
 async function generateTemplate(items: string[]): Promise<void> {
+  if (checkExists()){
+    console.error(".gitignore already exists");
+    Deno.exit(1);
+  }
+
   const template = await getTemplateContent(items);
   await Deno.writeTextFile("./.gitignore", template);
 }
@@ -17,6 +22,12 @@ function getUrl(items: string[]): string {
   const s = items.join(",");
   const url = `${BASE_URL}${s}`;
   return url;
+}
+
+
+function checkExists():boolean{
+  const file = Deno.statSync(".gitignore");
+  return file.isFile || file.isDirectory|| file.isSymlink;
 }
 
 generateTemplate(Deno.args);
